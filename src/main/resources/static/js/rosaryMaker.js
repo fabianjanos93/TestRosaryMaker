eecf4dfac21be158097ddf569051720d178762f6
Fábián János <fabian.janos93@gmail.com>
2019-01-11 16:40:04 +0100

    working thread and cross change

--- src/main/resources/static/js/rosaryMaker.js
+++ src/main/resources/static/js/rosaryMaker.js
@@ -1,7 +1,11 @@
 const rosaryImages = [];
+const threadImages =[];
+const displayThreadImages = [];
+const crossImages = [];
 const circles = [];
-
 const arrows = [];
+
+const threadImageSideSize = 420.688837;
 
 const arrowHeight = 40;
 const arrowWidth = 25;
@@ -15,51 +19,63 @@ const topArrowThreadColorChange = topArrowRosaryColorChange + arrowHeight + spac
 const leftArrowThreadColorChange = leftArrowRosaryColorChange;
 const rightArrowTheadColorChange = rightArrowRosaryColorChange;
 
+const topArrowCrossTypeChange = topArrowThreadColorChange + arrowHeight + spaceBetweenArrows;
+const leftArrowCrossTypeChange = leftArrowRosaryColorChange;
+const rightArrowCrossTypeChange = rightArrowRosaryColorChange;
+
+const topLeftCornerBallX = 132;
+const topRightCornerBallX = 392;
+const topCornerBallY = 54;
+
+const middleLeftCornerBallX = 52;
+const middleRightCornerBallX = 466;
+const middleCornerBallY = 310;
+
+const bottomCornerBallX = 259;
+const bottomCornerBallY = 466;
+
+const crossWitdh = 60;
+
+var thread;
+var cross;
+
 function setup() {
     function ballPlacement() {
         //bottom
         circles.push(
-            new RosaryBall(rosaryImages[0], 215, 800, 1),
-            new RosaryBall(rosaryImages[0], 215, 760, 2),
-            new RosaryBall(rosaryImages[0], 215, 740, 3),
-            new RosaryBall(rosaryImages[0], 215, 720, 4),
-            new RosaryBall(rosaryImages[0], 215, 680, 5),
+            new RosaryBall(ballPlaceholder, bottomCornerBallX, bottomCornerBallY+120, 1),
+            new RosaryBall(ballPlaceholder, bottomCornerBallX, bottomCornerBallY+80, 2),
+            new RosaryBall(ballPlaceholder, bottomCornerBallX, bottomCornerBallY+60, 3),
+            new RosaryBall(ballPlaceholder, bottomCornerBallX, bottomCornerBallY+40, 4),
+            new RosaryBall(ballPlaceholder, bottomCornerBallX, bottomCornerBallY, 5),
         );
         // dot
         circles.push(
-            new RosaryBall(rosaryImages[0], 120, 440, 16),
-            new RosaryBall(rosaryImages[0], 120, 140, 27),
-            new RosaryBall(rosaryImages[0], 310, 140, 38),
-            new RosaryBall(rosaryImages[0], 310, 440, 49),
+            new RosaryBall(ballPlaceholder, topLeftCornerBallX, topCornerBallY, 16),
+            new RosaryBall(ballPlaceholder, topRightCornerBallX, topCornerBallY, 27),
+            new RosaryBall(ballPlaceholder, middleLeftCornerBallX, middleCornerBallY, 38),
+            new RosaryBall(ballPlaceholder, middleRightCornerBallX, middleCornerBallY, 49),
         );
         //clockwise start left segment 1
         for (i = 0; i < 10; i++) {
-            circles.push(new RosaryBall(rosaryImages[0], 165 - 5 * i, 660 - i * 20, 6 + i))
+            circles.push(new RosaryBall(ballPlaceholder, ((11-i) * bottomCornerBallX + (i+2) * middleLeftCornerBallX)/13, ((11-i) * bottomCornerBallY + (i+2) * middleCornerBallY)/13, 6 + i))
         }
         //new segment 2
         for (i = 0; i < 10; i++) {
-            circles.push(new RosaryBall(rosaryImages[0], 120, 380 - i * 20, 17 + i))
+            circles.push(new RosaryBall(ballPlaceholder, ((11-i) * middleLeftCornerBallX + (i+2) * topLeftCornerBallX)/13, ((11-i) * middleCornerBallY + (i+2) * topCornerBallY)/13, 17 + i))
+        }
+        //new segment 3
+        for(i = 0; i< 10; i ++) {
+            circles.push(new RosaryBall(ballPlaceholder, ((11-i) * topLeftCornerBallX + (i+2) * topRightCornerBallX)/13, ((11-i) * topCornerBallY + (i+2) * topCornerBallY)/13, 53, 28*i));
         }
         //new segment 4
         for (i = 0; i < 10; i++) {
-            circles.push(new RosaryBall(rosaryImages[0], 310, 200 + i * 20, 39 + i))
+            circles.push(new RosaryBall(ballPlaceholder,((11-i) * topRightCornerBallX + (i+2) * middleRightCornerBallX)/13, ((11-i) * topCornerBallY + (i+2) * middleCornerBallY)/13, 39 + i))
         }
         //new segment 5
         for (i = 0; i < 10; i++) {
-            circles.push(new RosaryBall(rosaryImages[0], 310 - 5 * i, 480 + i * 20, 50 + i))
+            circles.push(new RosaryBall(ballPlaceholder, ((11-i) * middleRightCornerBallX + (i+2) * bottomCornerBallX)/13, ((11-i) * middleCornerBallY + (i+2) * bottomCornerBallY)/13, 50 + i))
         }
-        //new segment 3
-        circles.push(
-            new RosaryBall(rosaryImages[0], 120, 83, 28),
-            new RosaryBall(rosaryImages[0], 140, 71, 29),
-            new RosaryBall(rosaryImages[0], 160, 60, 30),
-            new RosaryBall(rosaryImages[0], 180, 45, 31),
-            new RosaryBall(rosaryImages[0], 203, 30, 32),
-            new RosaryBall(rosaryImages[0], 228, 30, 33),
-            new RosaryBall(rosaryImages[0], 250, 45, 34),
-            new RosaryBall(rosaryImages[0], 270, 60, 35),
-            new RosaryBall(rosaryImages[0], 290, 71, 36),
-            new RosaryBall(rosaryImages[0], 310, 83, 37));
 
         mainBall = new RosaryBall(rosaryImages[0], (leftArrowRosaryColorChange+arrowWidth+rightArrowRosaryColorChange)/2, topArrowRosaryColorChange+(arrowHeight/2), "Main Ball");
         mainBall.imagePointer = 0;
@@ -87,46 +103,103 @@ function setup() {
         var body = ('My permanent body contents');
         document.write('<a href="mailto:' + email + '?subject=' +subject+ '&body=' +body+ '">' + 'Click here to send email as well' + '<'+'/a>');
     }
-    let canvas = createCanvas(800,1000);
+    let canvas = createCanvas(800,800);
     let orderButton = createButton("Requesting an offer");
+
     orderButton.position(leftArrowRosaryColorChange,topArrowRosaryColorChange-spaceBetweenArrows);
     orderButton.mouseClicked(sendEmail);
 
-
     background(230);
+
+    threadImages.push(loadImage("images/baseThread.png"),
+                        loadImage("images/blueThread.png"),
+                        loadImage("images/redThread.png"));
+    thread = new Thread();
+    thread.imageIterateForward = function () {
+        if (thread.imagePointer === threadImages.length - 1) {
+            thread.imagePointer = 0;
+        } else {
+            thread.imagePointer++;
+        }
+        thread.changeImg(threadImages[thread.imagePointer])
+    };
+    thread.imageIterateBackward = function () {
+        if (thread.imagePointer === 0) {
+            thread.imagePointer = threadImages.length - 1;
+        } else {
+            thread.imagePointer--;
+        }
+        thread.changeImg(threadImages[thread.imagePointer])
+    };
+    thread.changeImg(threadImages[0]);
+
+    crossImages.push(loadImage("images/fullcross1.png"),
+                        loadImage("images/cross1.png"),
+                        loadImage("images/cross2.png"),
+                        loadImage("images/cross3.png"));
+    cross = new Cross();
+    cross.imageIterateForward = function () {
+        if (cross.imagePointer === crossImages.length - 1) {
+            cross.imagePointer = 0;
+        } else {
+            cross.imagePointer++;
+        }
+        cross.changeImg(crossImages[cross.imagePointer])
+    };
+    cross.imageIterateBackward = function () {
+        if (cross.imagePointer === 0) {
+            cross.imagePointer = crossImages.length - 1;
+        } else {
+            cross.imagePointer--;
+        }
+        cross.changeImg(crossImages[cross.imagePointer])
+    };
+    cross.changeImg(crossImages[0]);
+
+    ballPlaceholder = loadImage("images/ballPlaceholder.png");
 
     rosaryImages.push(loadImage("images/blueball.png"),
         redBall = loadImage("images/redball.png"),
-        greenBall = loadImage("images/greenball.png"))
+        greenBall = loadImage("images/greenball.png"));
 
     ballPlacement();
 
     arrows.push(new LeftArrow(leftArrowRosaryColorChange,topArrowRosaryColorChange,mainBall.imageIterateBackward),
-                new RightArrow(rightArrowRosaryColorChange,topArrowRosaryColorChange,mainBall.imageIterateForward,
-                    new LeftArrow(leftArrowThreadColorChange,topArrowThreadColorChange,)))
+                new RightArrow(rightArrowRosaryColorChange,topArrowRosaryColorChange,mainBall.imageIterateForward),
+                new LeftArrow(leftArrowThreadColorChange,topArrowThreadColorChange,thread.imageIterateBackward),
+                new RightArrow(rightArrowTheadColorChange,topArrowThreadColorChange,thread.imageIterateForward),
+                new LeftArrow(leftArrowCrossTypeChange,topArrowCrossTypeChange,cross.imageIterateForward),
+                new RightArrow(rightArrowCrossTypeChange,topArrowCrossTypeChange,cross.imageIterateBackward))
 }
 
 function draw() {
+
     function drawRosaryBalls() {
+
         circles.forEach(circle =>
             image(circle.image, circle.x, circle.y, circle.radius * 2, circle.radius * 2)
         );
     }
-
     function drawArrows() {
+
         arrows.forEach(arrow =>
             arrow.draw())
     }
 
     background(230);
+    image(thread.image,50,50,threadImageSideSize,threadImageSideSize);
+    image(thread.image,(leftArrowThreadColorChange+arrowWidth+rightArrowTheadColorChange)/2-15,topArrowThreadColorChange,30,(30/thread.image.width)*thread.image.height);
+    image(cross.image,bottomCornerBallX-(crossWitdh/2),bottomCornerBallY+145,crossWitdh,(crossWitdh/cross.image.width)*cross.image.height);
+    image(cross.image,(leftArrowCrossTypeChange+arrowWidth+rightArrowCrossTypeChange)/2-10,topArrowCrossTypeChange,20,(20/cross.image.width)*cross.image.height);
     drawRosaryBalls();
     drawArrows();
+
 }
 
 function mouseClicked() {
+    console.log("X: " + pmouseX + "| Y: " + pmouseY);
     circles.forEach(circle => {
         if (isIntersect(circle)) {
-            console.log(circle.id);
             circle.changeImg(mainBall.image);
         }
     });
@@ -156,6 +229,18 @@ function RosaryBall(image,posX,posY,id) {
     this.y = posY - this.radius;
     this.changeImg = function(toChange) {this.image = toChange};
     return this;
+}
+
+function Thread(){
+    this.imagePointer = 0;
+    this.image = threadImages[0];
+    this.changeImg = function(toChange) {this.image = toChange};
+}
+
+function Cross() {
+    this.imagePointer = 0;
+    this.image = crossImages[0];
+    this.changeImg = function(toChange) {this.image = toChange};
 }
 
 function LeftArrow(x,y,functionToRun){
